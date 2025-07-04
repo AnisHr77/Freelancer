134202bb288adff8728188a460204a449ab68e73
KaciAbdeldjalil <a_kaci@estin.dz>
2025-07-04 19:18:51 +0100

    add new changes

--- frontend/src/components/Home/Header.tsx
+++ frontend/src/components/Home/Header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { CiSearch } from "react-icons/ci";
 export default function Header() {
     return (
         <header className="w-full h-auto flex flex-col justify-around items-center lg:flex-row ">
@@ -9,18 +9,22 @@ export default function Header() {
                     The Ultimate Destination for <span className=" text-[#007AFF] ">freelance Talent</span> and <span className=" text-[#007AFF] " >Quality Services!</span>
                 </p>
                 <p className=" text-[gray] lg:max-w-[520px] lg:text-left  ">
-                    Whether you're a business owner or a freelancer,GigNotion is your one-stop-shop for findingor offering freelance services.
-                    Join our community todayand start getting thing done!
+                    Whether you're a business owner or a freelancer,
+                    GigNotion is your one-stop-shop for finding or offering freelance services.
+                    Join our community today and start getting things done!
                 </p>
-                <form className="flex flex-col gap-2  items-center   md:flex-row">
-                    <input
-                        type="search"
-                        placeholder="What are you looking for?"
-                        name="search-input"
-                        className="w-full  h-10 rounded-[5px] border outline-none pl-2"
-                        required
-                    />
-                    <button type="submit" className="w-20 h-10 rounded-[5px] bg-[#007AFF] text-white cursor-pointer hover:bg-white hover:text-[#007AFF] hover:border active:opacity-50">
+                <form className="flex flex-col gap-2 justify-center items-center  lg:justify-start md:flex-row">
+                    <div className="flex items-center border border-[gray] rounded-[5px] lg:w-90  w-80 ">
+                        <CiSearch className="w-6 h-6 ml-2 text-[gray]" />
+                        <input
+                            type="search"
+                            placeholder="What are you looking for?"
+                            name="search-input"
+                            className="w-full  h-10 outline-none pl-2 placeholder-[#b3b2b2] "
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="w-27 h-10 rounded-[5px] bg-[#007AFF] text-white cursor-pointer hover:bg-white hover:text-[#007AFF] hover:border active:opacity-50">
                         Search
                     </button>
                 </form>

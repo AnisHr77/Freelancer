6a7cd6868d57008159fb84c6da0b24bacf1bfd4f
Hadj Ramdane Anis <anis28hr@gmail.com>
2025-07-05 22:56:15 +0100

    fixing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -25,4 +25,3 @@ Route::apiResource('contracts', ContractController::class);
 Route::apiResource('messages', MessageController::class);
 Route::apiResource('reviews', ReviewController::class);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);

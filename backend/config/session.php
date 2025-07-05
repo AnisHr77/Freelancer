6e52fbfcfa779af5bd550c2ea555ef4fbbbdb756
Hadj Ramdane Anis <anis28hr@gmail.com>
2025-07-05 22:54:37 +0100

    fixing

--- backend/config/session.php
+++ backend/config/session.php
@@ -18,9 +18,7 @@ return [
     |
     */
 
-    // 'driver' => env('SESSION_DRIVER', 'database'),
-    'driver' => env('SESSION_DRIVER', 'file'),
-
+    'driver' => env('SESSION_DRIVER', 'database'),
 
     /*
     |--------------------------------------------------------------------------

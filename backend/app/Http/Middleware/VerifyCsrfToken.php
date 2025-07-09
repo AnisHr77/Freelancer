9341abac687d487f74620e43e1cfaca9e2c92fab
Anis <anis28hr@gmail.com>
2025-07-09 18:24:21 +0100

    some fixes in Dashboard

--- backend/app/Http/Middleware/VerifyCsrfToken.php
+++ backend/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,8 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'sanctum/csrf-cookie'
+        'login',
+        'api/login',
     ];
 
 

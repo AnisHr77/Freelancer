d10581c7d7eb66052cc9e7809cb9ab88788a1881
KaciAbdeldjalil <a_kaci@estin.dz>
2025-08-01 23:40:34 +0100

    modify question part

--- frontend/src/app/Blog/page.tsx
+++ frontend/src/app/Blog/page.tsx
@@ -53,26 +53,51 @@ export default function WhatWeOffer() {
         label: motivate.label,
     }))
     return (
-        <div className=" flex justify-between  ">
-            <div className=" flex flex-col justify-center items-center text-center ml-20 gap-12 mt-10 ">
-                <p className=" text-[30px] w-110 font-semibold " >Hey Adam. Ready for your
+        <div className=" flex   flex-col-reverse justify-between lg:flex-row ">
+            <div className=" flex flex-col justify-center items-center text-center lg:ml-20 gap-12 mt-10 pb-10 lg:pb-0 ">
+                <p className=" text-[25px] md:text-[35px] lg:text-[30px] w-full lg:w-110 font-semibold " >Hey Adam. Ready for your
                     next big opportunity?
                 </p>
-                <div className=" flex flex-col gap-4 ">
+                <div className=" flex flex-col gap-4 md:text-[25px] lg:text-[16px] ">
                     <p>Build a profile to show the world what you can do</p>
                     <hr></hr>
                     <p>Apply on jobs posted from clients, around the world</p>
                     <hr></hr>
                     <p>Get paid safely and know we’re here to help</p>
                 </div>
-
                 <button className=" w-80 h-10 rounded-[20px] text-white bg-[#7A4D8B] " >Create Your Profile</button>
             </div>
+
+            {/* desktop version */}
             <div
                 style={{ backgroundImage: `url('/wave2.png')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}
-                className="h-130 w-170 flex flex-col items-center pl-30 gap-2  "
+                className="h-130    flex-col items-center pl-30 gap-2 w-170  hidden lg:flex "
             >
                 <p className="  text-[white] ml-10 mt-10 text-[40px] ">
+                    <span className=' font-bold  ' >Task</span>linker
+                </p>
+                <img src={"/Frame.png"} className=" w-70 h-60 " />
+                <p className=" text-center w-90 text-white font-medium " >Join our community of talented freelancers and connect with clients from around the world.</p>
+            </div>
+
+            {/* phone version */}
+            <div
+                style={{ backgroundImage: `url('/wavephone.png')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}
+                className="h-140 w-full flex flex-col items-center   gap-2 md:hidden "
+            >
+                <p className="  text-[white]   mt-10 text-[40px] ">
+                    <span className=' font-bold  ' >Task</span>linker
+                </p>
+                <img src={"/Frame.png"} className=" w-70 h-60 " />
+                <p className=" text-center w-75 text-white font-medium " >Join our community of talented freelancers and connect with clients from around the world.</p>
+            </div>
+
+            {/* tablet version */}
+            <div
+                style={{ backgroundImage: `url('/wavetablet.png')`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}
+                className="h-140 w-full  flex-col items-center   gap-2 hidden md:flex lg:hidden "
+            >
+                <p className="  text-[white]   mt-10 text-[40px] ">
                     <span className=' font-bold  ' >Task</span>linker
                 </p>
                 <img src={"/Frame.png"} className=" w-70 h-60 " />
